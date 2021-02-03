f757a5b1ce6c553043f3c94f77c2a347da52b84c
arisbelky <ariselias2019@outlook.com>
2021-02-03 22:01:14 +0000

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,11 @@ int main(void)
 	long longType;
 	float floatType;
 
-	printf("Size of a char:\n" sizeof(charType);
-	printf("Size of a short:\n" sizeof(shortType);
-	printf("Size of int:\n" sizeof(intType);
-	printf("Size of long:\n" sizeof(longType);
-	printf("Size of float:\n" sizeof(floatType);
+	printf("Size of a char:%lubytes\n" sizeof(charType));
+	printf("Size of a short:%lubytes\n" sizeof(shortType));
+	printf("Size of int:%lu bytes\n" sizeof(intType));
+	printf("Size of long:%lu bytes\n" sizeof(longType));
+	printf("Size of float:%lu bytes\n" sizeof(floatType));
+	printf("Size of float:%lu bytes\n" sizeof(floatType));
 	return (0);
 }
