a08c15030ffcf4f5b8a7cad4fd16a2504e2d2b12
arisbelky <ariselias2019@outlook.com>
2021-02-03 15:34:56 +0000

    Using printf

---
+++ 4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main
+ * return 0
+ * puts
+ */
+int main()
+{
+	
+	return(0);
+}
---
+++ 5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *main 
+ *using printf
+ *return allways 0
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
