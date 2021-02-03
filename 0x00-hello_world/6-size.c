e1d7dad269192eaf796425472ff614165dccf5e7
arisbelky <ariselias2019@outlook.com>
2021-02-03 17:08:07 +0000

    Preprocessor

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main
+ * printing size of variables
+ * return allways 0
+ */
+int main()
+{
+	char charType;
+	short shortType;
+	int intType;
+	long longType;
+	float floatType;
+
+	printf("Size of a char:\n" sizeof(charType);
+	printf("Size of a short:\n" sizeof(shortType);
+	printf("Size of int:\n" sizeof(intType);
+	printf("Size of long:\n" sizeof(longType);
+	printf("Size of float:\n" sizeof(floatType);
+	return(0);
+}
