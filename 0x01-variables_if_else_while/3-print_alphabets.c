970404571a189c28c0adfd93946327b2e9b02349
arisbelky <ariselias2019@outlook.com>
2021-02-04 18:04:07 +0000

    Printing numbers

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -serves to start program
+ * Return: ends the execution
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'Z'; c++)
+		putchar(c);
+		putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -where the program begans
+ * Return: ends the execution
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	return (0);
+}
