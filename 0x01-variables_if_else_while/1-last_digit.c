21e329e6cc77bea5d4cecc6415fdb031145fb22e
arisbelky <ariselias2019@outlook.com>
2021-02-04 17:12:11 +0000

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 		}
 		else
 		{
-			printf("Last digit of %d is %d and is less than 6 but not 0\n", n, last);
+			printf("Last digit of %d is %d and is less than 6 not 0\n", n, last);
 		}
 	}
 	return (0);
