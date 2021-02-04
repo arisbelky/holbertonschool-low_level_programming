5d99d1751310ea663d44b4aba91fb5ecc0b5ef3d
arisbelky <ariselias2019@outlook.com>
2021-02-04 16:08:26 +0000

    alphabet on lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -serves as the starting point for program execution
+ * Return: ends the execution
+ */
+int main()
+{
+	char ch ='a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
+}
