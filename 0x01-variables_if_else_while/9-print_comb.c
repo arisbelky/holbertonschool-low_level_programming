f6844b063004ff955d4190e413858d0294171e97
arisbelky <ariseliasgael2019@outlook.com>
2021-02-04 23:43:30 +0000

    printing with comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -start the program true there
+ * Return: end the execution
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
