49b5d8331cbec0f79ac1f089a467cab82020ceb3
arisbelky <ariseliasgael2019@outlook.com>
2021-02-05 13:04:23 +0000

    printing from 012 to 789

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -where the program starts
+ * Return: ends the execution
+ */
+int main(void)
+{
+	int i, n, a;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (n = '1'; n <= '8'; n++)
+		{
+			for (a = '2'; a <= '9'; a++)
+			{
+				putchar(i);
+				putchar(n);
+				putchar(a);
+				if (i < '7' || n < '8' || a < '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
