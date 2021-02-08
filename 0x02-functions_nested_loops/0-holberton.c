18dfb6ae9fcad4d9b3533f07ed502d946dd2bf85
arisbelky <ariseliasgael2019@outlook.com>
2021-02-08 14:17:25 +0000

    using _putchar to print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 #include <stdio.h>
 /**
  * holberton libraries
@@ -6,6 +7,15 @@
  */
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,4 @@
+/**prototype for main*/
+void print_alphabet(void);
+/**Functions*/
+int _putchar(char c);
