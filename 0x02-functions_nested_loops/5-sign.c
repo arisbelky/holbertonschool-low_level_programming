c718a8165100f14151c67a69e944ab47b646d61b
arisbelky <ariseliasgael2019@outlook.com>
2021-02-09 13:51:27 +0000

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_sign - start the program
+ * @n: return a interger and sign
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n> 0 )
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+	}
+	return (3);
+}
