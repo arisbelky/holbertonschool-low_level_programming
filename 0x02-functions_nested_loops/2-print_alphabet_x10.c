0aad128b048be0defe18677df9a4981512f08da7
arisbelky <ariseliasgael2019@outlook.com>
2021-02-09 12:56:51 +0000

    Printing alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void)
 {
 	char n;
 	int i;
-	
+
 	i = 1;
 	while (i < 11)
 	{
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _islower - Starts the program
+ * Return: 0 or 1
+ * @c: return a number
+ */
+int _islower(int c)
+{
+	int e = 97;
+	int g = 122;
+
+	if ( )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
